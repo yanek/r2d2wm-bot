3973ddf4ed93e4da0e5a09961305fd95940d3326
Noé Ksiazek <noe@ksiazek.xyz>
2024-06-25 00:30:02 +0200

    Move Handler into bot.rs -- at least for now

--- r2d2wm-bot/src/bot.rs
+++ r2d2wm-bot/src/bot.rs
@@ -1,12 +1,13 @@
 use crate::config::Config;
-use crate::handler::Handler;
 use crate::Result;
+use serenity::all::{Context, EventHandler, Ready};
+use serenity::async_trait;
 use serenity::prelude::*;
 
 pub async fn run(config: &Config) -> Result<()> {
     let intents = GatewayIntents::non_privileged();
 
-    tracing::info!("Creating client...");
+    tracing::debug!("Creating client...");
     let mut client = Client::builder(&config.app.discord_token, intents)
         .event_handler(Handler)
         .await?;
@@ -14,4 +15,13 @@ pub async fn run(config: &Config) -> Result<()> {
 
     client.start().await?;
     Ok(())
+}
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        tracing::info!("{} is connected!", ready.user.name);
+    }
 }
