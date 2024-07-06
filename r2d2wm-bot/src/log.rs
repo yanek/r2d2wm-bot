ea6e186411b0e9b53f7357c46b1fe74e4071061f
Noé Ksiazek <noe@ksiazek.xyz>
2024-07-06 21:32:55 +0200

    Clippy

--- r2d2wm-bot/src/log.rs
+++ r2d2wm-bot/src/log.rs
@@ -1,12 +1,10 @@
 use std::env;
 
-const DEFAULT_LOG_LEVEL: &str = "warn";
-const PACKAGE_NAME: &str = "r2d2wm_bot";
-
 pub fn init() {
     let filter = env::var("RUST_LOG").unwrap_or("trace".to_string());
     tracing_subscriber::fmt()
         .with_env_filter(&filter)
+        .without_time()
         .with_line_number(true)
         .init();
     tracing::debug!("Logging initialized with level: {filter:?}");
